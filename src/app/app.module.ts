32ffaf580b597375cba97391a7d3314548cc97a2
baby7305 <songhaibei7305@163.com>
2018-11-20 14:32:00 +0800

    新建basic、multiple 2个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { BasicComponent } from './combogrid/basic/basic.component';
+import { MultipleComponent } from './combogrid/multiple/multiple.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +19,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    BasicComponent,
+    MultipleComponent
   ],
   imports: [
     FormsModule,
