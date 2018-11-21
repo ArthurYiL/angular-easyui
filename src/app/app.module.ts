191921912c16832fa145f5b6a132b20cd339926a
baby7305 <songhaibei7305@163.com>
2018-11-21 09:55:49 +0800

    新建dialog-editing、edit-form 2个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { DialogEditingComponent } from './datagrid/dialog-editing/dialog-editing.component';
+import { EditFormComponent } from './datagrid/edit-form/edit-form.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +19,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    DialogEditingComponent,
+    EditFormComponent
   ],
   imports: [
     FormsModule,
---
+++ src/app/datagrid/dialog-editing/dialog-editing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-editing',
+  templateUrl: './dialog-editing.component.html',
+  styleUrls: ['./dialog-editing.component.css']
+})
+export class DialogEditingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
