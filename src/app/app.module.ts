003f8de62fba4333edd64d89bd5f3b6a18e42aa1
baby7305 <songhaibei7305@163.com>
2018-11-21 13:03:35 +0800

    新建basic、shopping 2个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { BasicComponent } from './droppable/basic/basic.component';
+import { ShoppingComponent } from './droppable/shopping/shopping.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +19,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    BasicComponent,
+    ShoppingComponent
   ],
   imports: [
     FormsModule,
