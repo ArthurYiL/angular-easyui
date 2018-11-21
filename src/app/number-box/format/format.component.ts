6268fafeca9f8a99b924eb360880678769b588f2
baby7305 <songhaibei7305@163.com>
2018-11-21 17:32:21 +0800

    number-box demo配置

--- src/app/number-box/format/format.component.ts
+++ src/app/number-box/format/format.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormatComponent implements OnInit {
 
+  numbers = [{
+    label: 'Number in the United States',
+    groupSeparator: ',',
+    decimalSeparator: '.'
+  }, {
+    label: 'Number in France',
+    groupSeparator: ' ',
+    decimalSeparator: ','
+  }, {
+    label: 'Currency:USD',
+    groupSeparator: ',',
+    decimalSeparator: '.',
+    prefix: '$'
+  }, {
+    label: 'Currency:EUR',
+    groupSeparator: ',',
+    decimalSeparator: ' ',
+    prefix: '€'
+  }, {
+    label: 'Currency:EUR',
+    groupSeparator: ' ',
+    decimalSeparator: ',',
+    suffix: '€'
+  }];
+
   constructor() { }
 
   ngOnInit() {
