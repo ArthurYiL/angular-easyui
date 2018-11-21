6003c051d009e1d8009fb641744fa0ac0e25bfd6
baby7305 <songhaibei7305@163.com>
2018-11-21 13:52:23 +0800

    新建reactive、template-driven 2个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { ReactiveComponent } from './form/reactive/reactive.component';
+import { TemplateDrivenComponent } from './form/template-driven/template-driven.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +19,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    ReactiveComponent,
+    TemplateDrivenComponent
   ],
   imports: [
     FormsModule,
