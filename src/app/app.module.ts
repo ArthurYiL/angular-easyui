08194e4247e8d604e37cdc5c535755f0e2f3923c
baby7305 <songhaibei7305@163.com>
2018-11-21 11:13:16 +0800

    新建basic、format、template 3个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { BasicComponent } from './datebox/basic/basic.component';
+import { FormatComponent } from './datebox/format/format.component';
+import { TemplateComponent } from './datebox/template/template.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +20,10 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    BasicComponent,
+    FormatComponent,
+    TemplateComponent
   ],
   imports: [
     FormsModule,
