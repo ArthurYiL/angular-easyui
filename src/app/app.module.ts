9ba75d32333d9deecd2c43bd4ada69b67a7df176
baby7305 <songhaibei7305@163.com>
2018-11-22 14:34:59 +0800

    新建basic、check-box、filtering、lazy-load、node-icon、template 6个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { BasicComponent } from './tree/basic/basic.component';
+import { CheckBoxComponent } from './tree/check-box/check-box.component';
+import { NodeIconComponent } from './tree/node-icon/node-icon.component';
+import { LazyLoadComponent } from './tree/lazy-load/lazy-load.component';
+import { TemplateComponent } from './tree/template/template.component';
+import { FilteringComponent } from './tree/filtering/filtering.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +23,13 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    BasicComponent,
+    CheckBoxComponent,
+    NodeIconComponent,
+    LazyLoadComponent,
+    TemplateComponent,
+    FilteringComponent
   ],
   imports: [
     FormsModule,
---
+++ src/app/tree/node-icon/node-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node-icon',
+  templateUrl: './node-icon.component.html',
+  styleUrls: ['./node-icon.component.css']
+})
+export class NodeIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
