98c6da3dee84136f098630ee8c750923a86699f5
baby7305 <songhaibei7305@163.com>
2018-11-22 10:00:42 +0800

    新建basic、style 2个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { BasicComponent } from './side-menu/basic/basic.component';
+import { StyleComponent } from './side-menu/style/style.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +19,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    BasicComponent,
+    StyleComponent
   ],
   imports: [
     FormsModule,
