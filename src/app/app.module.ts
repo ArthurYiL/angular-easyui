b5df2d0b439d22bcd62640eec7ee6b048a701452
baby7305 <songhaibei7305@163.com>
2018-11-22 09:51:24 +0800

    新建basic、category 2个模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { EasyUIModule } from 'ng-easyui/components/easyui/easyui.module';
 
 import { AppComponent } from './app.component';
 import { OfficialComponent } from './official/official.component';
+import { BasicComponent } from './search-box/basic/basic.component';
+import { CategoryComponent } from './search-box/category/category.component';
 
 const appRoutes: Routes = [
   { path: '', component: OfficialComponent },
@@ -17,7 +19,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    OfficialComponent
+    OfficialComponent,
+    BasicComponent,
+    CategoryComponent
   ],
   imports: [
     FormsModule,
