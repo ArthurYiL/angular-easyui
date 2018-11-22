6e9b397d23d63b43a53943d057dc835af7ace90d
baby7305 <songhaibei7305@163.com>
2018-11-22 10:18:05 +0800

    side-menu demo配置

--- src/app/side-menu/basic/basic.component.ts
+++ src/app/side-menu/basic/basic.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicComponent implements OnInit {
 
+  width = 200;
+  collapsed = false;
+  selection = null;
+  menus = [{
+    text: 'Item1',
+    iconCls: 'icon-sum',
+    state: 'open',
+    children: [{
+      text: 'Option1'
+    }, {
+      text: 'Option2'
+    }, {
+      text: 'Option3',
+      children: [{
+        text: 'Option31'
+      }, {
+        text: 'Option32'
+      }]
+    }]
+  }, {
+    text: 'Item2',
+    iconCls: 'icon-more',
+    children: [{
+      text: 'Option4'
+    }, {
+      text: 'Option5'
+    }, {
+      text: 'Option6'
+    }]
+  }];
+
+  toggle() {
+    this.collapsed = !this.collapsed;
+    this.width = this.collapsed ? 50 : 200;
+  }
+
   constructor() { }
 
   ngOnInit() {
--- src/app/side-menu/style/style.component.ts
+++ src/app/side-menu/style/style.component.ts
@@ -1,11 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-style',
   templateUrl: './style.component.html',
-  styleUrls: ['./style.component.css']
+  styleUrls: ['./style.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class StyleComponent implements OnInit {
+
+  width = 200;
+  collapsed = false;
+  menus = [{
+    text: 'Forms',
+    iconCls: 'fa fa-wpforms',
+    state: 'open',
+    children: [{
+      text: 'Form Element'
+    }, {
+      text: 'Wizard'
+    }, {
+      text: 'File Upload'
+    }]
+  }, {
+    text: 'Mail',
+    iconCls: 'fa fa-at',
+    selected: true,
+    children: [{
+      text: 'Inbox'
+    }, {
+      text: 'Sent'
+    }, {
+      text: 'Trash',
+      children: [{
+        text: 'Item1'
+      }, {
+        text: 'Item2'
+      }]
+    }]
+  }, {
+    text: 'Layout',
+    iconCls: 'fa fa-table',
+    children: [{
+      text: 'Panel'
+    }, {
+      text: 'Accordion'
+    }, {
+      text: 'Tabs'
+    }]
+  }];
+
+  toggle() {
+    this.collapsed = !this.collapsed;
+    this.width = this.collapsed ? 50 : 200;
+  }
 
   constructor() { }
 
