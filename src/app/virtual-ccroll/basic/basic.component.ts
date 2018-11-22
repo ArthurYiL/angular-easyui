00b93a7c8d1e70e50f8f1cf6537d2a6353c2c68e
baby7305 <songhaibei7305@163.com>
2018-11-22 16:03:18 +0800

    virtual-scroll demo配置

--- src/app/virtual-ccroll/basic/basic.component.ts
+++ src/app/virtual-ccroll/basic/basic.component.ts
@@ -9,7 +9,22 @@ export class BasicComponent implements OnInit {
 
   constructor() { }
 
+  height: number = 250;
+  total: number = 10000;
+  data: any[] = [];
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  rowHeight: number = 60;
+
   ngOnInit() {
+    for (let i = 0; i < this.total; i++) {
+      this.data.push({
+        id: i,
+        name: 'name' + i,
+        addr: 'address' + i,
+        desp: 'description' + i
+      });
+    }
   }
 
 }
--- src/app/virtual-ccroll/lazy-load/lazy-load.component.ts
+++ src/app/virtual-ccroll/lazy-load/lazy-load.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LazyLoadComponent implements OnInit {
 
+  lazy: boolean = true;
+  height: number = 250;
+  total: number = 10000;
+  data: any[] = [];
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  rowHeight: number = 60;
+
+  onPageChange(event) {
+    setTimeout(() => {
+      this.pageNumber = event.pageNumber;
+      let data = [];
+      let start = (this.pageNumber - 1) * this.pageSize;
+      for (let i = start; i < start + this.pageSize; i++) {
+        data.push({
+          id: i,
+          name: 'name' + i,
+          addr: 'address' + i,
+          desp: 'description' + i
+        });
+      }
+      this.data = data;
+    }, 250);
+  }
+
   constructor() { }
 
   ngOnInit() {
