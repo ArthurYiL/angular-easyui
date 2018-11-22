32323f169faa51f9260414b5243d5e25cbfa65d1
baby7305 <songhaibei7305@163.com>
2018-11-22 09:33:44 +0800

    progress-bar demo配置

--- src/app/progress-bar/basic/basic.component.ts
+++ src/app/progress-bar/basic/basic.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicComponent implements OnInit {
 
+  value = 0;
+  disabled = false;
+
+  run() {
+    this.disabled = true;
+    let timeout = Math.random() * 500;
+    setTimeout(() => {
+      this.value += Math.floor(Math.random() * 10);
+      if (this.value > 100) {
+        this.value = 100;
+        this.disabled = false;
+      } else {
+        this.run();
+      }
+    }, timeout);
+  }
+
   constructor() { }
 
   ngOnInit() {
