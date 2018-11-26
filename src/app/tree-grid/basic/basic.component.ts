57fd9b18301d633d4fbf5f1643a7d63b2cf592a1
baby7305 <songhaibei7305@163.com>
2018-11-26 16:11:45 +0800

    5分钟的demo，title，grid，polar、angleAxis 2个图、radar

--- src/app/tree-grid/basic/basic.component.ts
+++ src/app/tree-grid/basic/basic.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
-  styleUrls: ['./basic.component.css']
+  styleUrls: ['./basic.component.scss',
+    './basic.component.css']
 })
 export class BasicComponent implements OnInit {
+
+  options: any;
 
   constructor() { }
 
   ngOnInit() {
+    // 指定图表的配置项和数据
+    this.options = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+
   }
 
 }
--- src/app/tree-grid/frozen-column/frozen-column.component.ts
+++ src/app/tree-grid/frozen-column/frozen-column.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrozenColumnComponent implements OnInit {
 
+  options1: any;
+  options2: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.options1 = {
+      polar: {},
+      angleAxis: {
+        min: 0,
+        max: 360,
+        interval: 30,
+        startAngle: 45
+      },
+      radiusAxis: {
+        min: 0,
+        max: 10,
+        interval: 2
+      },
+      series: []
+    };
+
+    this.options2 = {
+      polar: {},
+      angleAxis: {
+        min: 0,
+        max: 360,
+        interval: 30,
+        clockwise: false
+      },
+      radiusAxis: {
+        min: 0,
+        max: 10,
+        interval: 2
+      },
+      series: []
+    };
   }
 
 }
